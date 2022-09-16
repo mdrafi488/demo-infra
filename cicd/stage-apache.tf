39f11084586fcb220b0496230ca374b0d2d9f708
mdrafi488 <mdrafi.f488@gmail.com>
2022-09-16 22:48:00 +0530

    First commit

---
+++ cicd/stage-apache.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "apache-sg" {
+  name        = "apache-sg"
+  description = "Admin with ssh"
+  vpc_id      = "vpc-049f215e85a9ff2c2"
+
+  ingress {
+    dscription      = "admin"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    dscription      = "Enduser"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "apache-sg"
+  }
+}
+
+resource "aws_instance" "apache" {
+  ami           = "ami-0b89f7b3f054b957e"
+  instance_type = "t2.micro"
+  vpc_security_group_ids=[aws_security_group.apache-sg.id]
+  subnet_id = "subnet-072762878afe76c41"
+  key_name = aws_key_pair.demo.id
+
+  tags = {
+    Name = "apache"
+  }
+}
+

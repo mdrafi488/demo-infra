bf3374b2e86af3da855a306bece6028e9d177207
mdrafi488 <mdrafi.f488@gmail.com>
2022-09-17 15:49:58 +0530

    9th time commited

--- provider.tf
+++ provider.tf
@@ -9,3 +9,16 @@ terraform {
 provider "aws" {
   # Configuration options
 }
+/* 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+} */

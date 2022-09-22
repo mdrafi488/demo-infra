ee3294538efec07aafc980f954863bd8e2867fc6
mdrafi488 <mdrafi.f488@gmail.com>
2022-09-22 20:27:55 +0530

    commited

--- s3.tf
+++ s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucket-rafi-nlr1" {
-  bucket = "bucket-rafi-nlr"
+  bucket = "bucket-rafi-nlr1"
   acl    = "private"
 
   tags = {
-    Name        = "bucket-rafi-nlr"
+    Name        = "bucket-rafi-nlr1"
     Environment = "Dev"
   }
 }
